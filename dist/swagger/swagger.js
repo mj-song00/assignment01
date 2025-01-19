0c1c1509cee810bea88eb14edb4cb45ead3a9293
mj-song00 <niveus128@gmail.com>
2025-01-19 17:52:44 +0900

    Chore: swagger.js 추가

---
+++ dist/swagger/swagger.js
@@ -0,0 +1,107 @@
+"use strict";
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "한달인턴 과제 API",
+            description: "한달인턴 과제 API입니다.",
+            contact: {
+                name: "송민지",
+                email: "niveus128@gmail.com",
+                // url: "https://mixedcode.com",
+            },
+            version: "1.0.0",
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+                // description: "Local Development",
+            },
+            // {
+            //   url: "http://test.co.kr/",
+            //   description: "Test Server",
+            // },
+            // {
+            //   url: "http://real.co.kr/",
+            //   description: "Real Server",
+            // },
+        ],
+        components: {
+            schemas: {
+                User: {
+                    type: "object",
+                    properties: {
+                        username: {
+                            type: "string",
+                            description: "사용자 닉네임",
+                            example: "송민지",
+                        },
+                        nickname: {
+                            type: "string",
+                            description: "nickname",
+                            example: "모코코",
+                        },
+                        password: {
+                            type: "string",
+                            description: "비밀번호",
+                            example: "Asdf1234!",
+                        },
+                    },
+                },
+                LoginDto: {
+                    type: "object",
+                    properties: {
+                        username: {
+                            type: "string",
+                            description: "사용자 닉네임",
+                            example: "송민지",
+                        },
+                        password: {
+                            type: "string",
+                            description: "비밀번호",
+                            example: "Asdf1234!",
+                        },
+                    },
+                },
+                ErrorResponse: {
+                    type: "object",
+                    properties: {
+                        message: {
+                            type: "string",
+                            description: "에러 메세지",
+                            example: "user not found",
+                        },
+                        statusCode: {
+                            type: "integer",
+                            description: "http code",
+                            example: 400,
+                        },
+                    },
+                },
+                Responses: {
+                    type: "object",
+                    properties: {
+                        message: {
+                            type: "string",
+                            description: "요청 완료",
+                            example: "success",
+                        },
+                        statusCode: {
+                            type: "integer",
+                            description: "http code",
+                            example: 200,
+                        },
+                    },
+                },
+            },
+        },
+    },
+    apis: ["src/routes/*.ts", "./swagger/*"],
+};
+const specs = swaggerJsdoc(options);
+module.exports = {
+    swaggerUi,
+    specs,
+};
