69d12dd96bebec31e71adf03ef3cdd55917c3814
mj-song00 <niveus128@gmail.com>
2025-01-19 20:55:41 +0900

    Chore: 스웨거 js루트 수정

--- dist/app.js
+++ dist/app.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const user_route_1 = __importDefault(require("./src/routes/user.route"));
 const cors = require("cors");
-const { swaggerUi, specs } = require("./swagger/swagger");
+const { swaggerUi, specs } = require("./swagger");
 const app = (0, express_1.default)();
 const port = 3000;
 app.use(express_1.default.json());
